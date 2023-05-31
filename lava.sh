213af6296ea6ed5a4bb824c72a1cfdd083e88c1b
dmsv312 <107628203+dmsv312@users.noreply.github.com>
2023-05-31 13:17:17 +0700

    Create lava.sh

---
+++ lava.sh
@@ -0,0 +1,48 @@
+ver="1.19" && \
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
+sudo rm -rf /usr/local/go && \
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
+rm "go$ver.linux-amd64.tar.gz" && \
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
+source $HOME/.bash_profile && \
+go version
+git clone https://github.com/lavanet/lava && cd lava
+git checkout v0.11.2
+make install
+lavad init $NODENAME --chain-id lava-testnet-1
+cd $HOME
+git clone https://github.com/K433QLtr6RA9ExEq/GHFkqmTzpdNLDd6T
+cp $HOME/GHFkqmTzpdNLDd6T/testnet-1/genesis_json/genesis.json $HOME/.lava/config
+rm -r GHFkqmTzpdNLDd6T/
+wget -O $HOME/.lava/config/addrbook.json "https://share2.utsa.tech/lava/addrbook.json"
+lavad config chain-id lava-testnet-1
+lavad config keyring-backend os
+sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.0025ulava\"/;" ~/.lava/config/app.toml
+external_address=$(wget -qO- eth0.me)
+sed -i.bak -e "s/^external_address *=.*/external_address = \"$external_address:26656\"/" $HOME/.lava/config/config.toml
+peers=""
+sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" $HOME/.lava/config/config.toml
+seeds="3a445bfdbe2d0c8ee82461633aa3af31bc2b4dc0@prod-pnet-seed-node.lavanet.xyz:26656,e593c7a9ca61f5616119d6beb5bd8ef5dd28d62d@prod-pnet-seed-node2.lavanet.xyz:26656"
+sed -i.bak -e "s/^seeds =.*/seeds = \"$seeds\"/" $HOME/.lava/config/config.toml
+snapshot_interval=1000
+sed -i.bak -e "s/^snapshot-interval *=.*/snapshot-interval = \"$snapshot_interval\"/" ~/.lava/config/app.toml
+cp $HOME/.lava/data/priv_validator_state.json $HOME/.lava/priv_validator_state.json.backup
+curl -L https://share2.utsa.tech/lava/snap-lava.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.lava --strip-components 2
+mv $HOME/.lava/priv_validator_state.json.backup $HOME/.lava/data/priv_validator_state.json
+tee /etc/systemd/system/lavad.service > /dev/null <<EOF
+[Unit]
+Description=lavad
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which lavad) start
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable lavad
+systemctl restart lavad && journalctl -u lavad -f -o cat
