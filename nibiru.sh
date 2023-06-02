3b6303cb60c402d33adc0360c637c4e26d43dd82
dmsv312 <107628203+dmsv312@users.noreply.github.com>
2023-06-02 10:38:43 +0700

    Update nibiru.sh

--- nibiru.sh
+++ nibiru.sh
@@ -1,6 +1,6 @@
 apt update && apt upgrade -y
 apt install curl iptables build-essential git wget jq make gcc nano tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev -y
-VALIDATOR="nodesUP"
+VALIDATOR="n83707"
 BINARY=nibid
 CHAIN_ID=nibiru-itn-1
 cd $HOME
